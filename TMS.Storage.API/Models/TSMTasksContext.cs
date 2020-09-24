77eb5dc19d2020c21d1e49edc824bae1cbae454d
Mario Olivera <mariomalaga94@gmail.com>
2020-09-24 21:01:29 +0100

    First commit project, connection with SQL Server and CRUD

---
+++ TMS.Storage.API/Models/TSMTasksContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+namespace API.Models
+{
+    public partial class TSMTasksContext : DbContext
+    {
+        public TSMTasksContext()
+        {
+        }
+
+        public TSMTasksContext(DbContextOptions<TSMTasksContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Tsmtask> Tsmtask { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Tsmtask>(entity =>
+            {
+                entity.ToTable("TSMTask");
+
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.Description)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.FinishDate).HasColumnType("datetime");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.StartDate).HasColumnType("datetime");
+
+                entity.Property(e => e.State)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ TMS.Storage.API/Models/Tsmtask.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace API.Models
+{
+    public partial class Tsmtask
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? FinishDate { get; set; }
+        public string State { get; set; }
+    }
+}
